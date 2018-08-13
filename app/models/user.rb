3e1c17a920ace384fd674058d6103e9758c2049b
Constantin Wischnath <constantinwischnath@hotmail.com>
2018-08-13 16:12:20 +0200

    redo models, add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Equipment user owns
+  has_many :equipments
+  has_many :bookings
+  # Equipment user rents
+  has_many :rented_equipments, through: :bookings, source: :equipment
 end
--- db/migrate/20180813140158_create_bookings.rb
+++ db/migrate/20180813140158_create_bookings.rb
@@ -5,7 +5,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
       t.references :equipment, foreign_key: true
       t.date :starts_at
       t.date :ends_at
-      t.string :status
+      t.string :status, default: "pending", null: false
 
       t.timestamps
     end
