d6adaf5d73be1583cdb31af76198d4cb9e0e5b62
Constantin Wischnath <constantinwischnath@hotmail.com>
2018-08-13 14:55:35 +0200

    sportsX

---
+++ db/migrate/20180813125217_create_equipment.rb
@@ -0,0 +1,15 @@
+class CreateEquipment < ActiveRecord::Migration[5.2]
+  def change
+    create_table :equipment do |t|
+      t.string :category
+      t.string :brand
+      t.string :product_name
+      t.string :size
+      t.string :location
+      t.integer :price_per_day
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20180813125408_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.references :user, foreign_key: true
+      t.references :equipment, foreign_key: true
+      t.date :starts_at
+      t.date :ends_at
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
