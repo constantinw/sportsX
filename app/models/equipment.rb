00c430c9a81f99fe42fccfaa9161871b764e92ba
Jinyong Kim <o2.3ys.v.er@gmail.com>
2018-08-15 18:53:12 +0200

    add geocoding

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -9,4 +9,7 @@ class Equipment < ApplicationRecord
   validates :location, presence: true
   validates :price_per_day, presence: true
   validates :photo, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
