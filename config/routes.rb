164a48c61d2d2394132c8514fc7500c05c2f1a59
Nicolas Schorlo <n.vonschorlemer@whu.edu>
2018-08-15 14:30:53 +0200

    created and connected the about page, blanko page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/about" => "pages#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :equipments, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:create, :index]
   end
+
 end
 
