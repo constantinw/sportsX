88abb91d05f385d5d16fc72db915c459140f72ec
Jinyong Kim <o2.3ys.v.er@gmail.com>
2018-08-15 22:17:42 +0200

    category controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "/about" => "pages#about"
 
+  get "/air" => "categories#air"
+  get "/land" => "categories#land"
+  get "/water" => "categories#water"
+  get "/groups" => "categories#groups"
+
   resources :equipments, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:create]
   end
+
   resources :bookings, only: [:index]
 end
 
