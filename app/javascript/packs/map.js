0eedd048399e12dc85a9319d5986084f726025da
Constantin Wischnath <constantinwischnath@hotmail.com>
2018-08-16 17:51:12 +0200

    map is working

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -5,16 +5,6 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
-  } else {
-    map.fitLatLngBounds(markers);
-  }
-}
-
 const styles = [
     {
         "featureType": "all",
@@ -44,3 +34,13 @@ map.addStyle({
   mapTypeId: 'map_style'
 });
 map.setStyle('map_style');
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(14);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+}
+
